cac23991d640ff6febc0013d8d88b9610c73a53d
EstherWanjiruUge <ugeesther@gmail.com>
2022-08-07 09:13:20 -0700

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    const thankyous = [];
+    for (let i = 0; i < names.length; i++) {
+        const string = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        thankyous.push(string)
+       
+    }
+    return thankyous;
+}
+
+function countDown(num){
+    for(let i = num;i >= 0; i--){
+        console.log(i)
+    }
+}
